c7f012140efbef83c3928b8c9eb63e3d0dc4ac77
andrei <andreimandrique3@gmail.com>
2024-10-07 21:28:06 +0800

    place random ship

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const board = [
 ];
 //0 1 2 3 4 5 6 7 8 9         X
 
-function placeCarrier() {
+function placeRandomCarrier() {
   while (true) {
     const randomP = randomPosition();
     const randomD = randomDirection();
@@ -24,8 +24,43 @@ function placeCarrier() {
     const startPosition = carrier.position(randomP);
     const { endPosition, fullPath } = carrier.path(randomD);
 
-    if (endPosition[0] < 10 && endPosition[1] < 10) {
+    if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
       return fullPath;
     }
   }
+}
+
+function placeRandomBattleship() {
+  while (true) {
+    const randomP = randomPosition();
+    const randomD = randomDirection();
+
+    const carrier = new Ship("battleship", 4);
+    const startPosition = carrier.position(randomP);
+    const { endPosition, fullPath } = carrier.path(randomD);
+
+    if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
+      return fullPath;
+    }
+  }
+}
+
+const occupied = new Set();
+
+function isPathClear(path) {
+  for (const position of path) {
+    if (occupied.has(position)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const randomC = placeRandomCarrier();
+const randomB = placeRandomBattleship();
+
+const combine = [...randomC, ...randomB];
+
+for (const item of combine) {
+  console.log(item);
 }
