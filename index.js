44f4b0c83e6ef0947832074f49e62ed747df194d
andrei <andreimandrique3@gmail.com>
2024-10-10 15:15:08 +0800

    working gui

--- index.js
+++ index.js
@@ -1,12 +1,77 @@
-import {
-  randomShipPosition,
-  carrierRandomPosition,
-  shipInformation,
-} from "./randomShip.js";
+import { makeRandomShipPosition } from "./randomShip.js";
+import { randomPosition } from "./random.js";
 
-const computerContainer = document.querySelector(".computer-container");
+function makeRandomShip() {
+  const { randomCarrier } = makeRandomShipPosition();
 
-const computerBoard = [
+  return randomCarrier;
+}
+
+// const computerContainer = document.querySelector(".computer-container");
+const playerContainer = document.querySelector(".player-container");
+const btn = document.querySelector("button");
+
+// const computerBoard = [
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 0
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 2
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 3
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 4   Y
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 5
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 6
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 7
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 8
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 9
+// ];
+
+// const computerRandomShip = makeRandomShip();
+// const computerShipPosition = new Set();
+// const computerAllMoves = new Set();
+
+// for (const ship of computerRandomShip) {
+//   const x = ship[0];
+//   const y = ship[1];
+//   computerBoard[x][y] = "b";
+//   computerShipPosition.add([x, y].toString());
+// }
+
+// for (let y = 0; y < 10; y++) {
+//   for (let x = 0; x < 10; x++) {
+//     const div = document.createElement("div");
+//     div.classList.add("cell");
+
+//     if (computerBoard[x][y] == "b") {
+//       div.classList.add("blue");
+//     }
+
+//     div.addEventListener("click", () => {
+//       if (computerShipPosition.has([x, y].toString())) {
+//         if (computerAllMoves.has([x, y].toString())) {
+//           // console.log("cannot attack the same position twice");
+//           attackRandomPosition(false);
+//         } else {
+//           div.classList.add("red");
+//           // console.log("hit");
+//           attackRandomPosition(true);
+//         }
+//       } else {
+//         if (computerAllMoves.has([x, y].toString())) {
+//           attackRandomPosition(false);
+//         } else {
+//           div.classList.add("green");
+//           // console.log("miss");
+//           attackRandomPosition(true);
+//         }
+//       }
+
+//       computerAllMoves.add([x, y].toString());
+//     });
+
+//     computerContainer.append(div);
+//   }
+// }
+
+const playerBoard = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 0
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 2
@@ -19,35 +84,60 @@ const computerBoard = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 9
 ];
 
-const randomShip = carrierRandomPosition();
-const computerShipPosition = new Set();
+const playerRandomShip = makeRandomShip();
+const playerAllShipPosition = new Set();
+const playerAllMoves = new Set();
 
-for (const ship of randomShip) {
+for (const ship of playerRandomShip) {
   const x = ship[0];
   const y = ship[1];
-  computerBoard[x][y] = 1;
-  computerShipPosition.add([x, y].toString());
+  playerBoard[x][y] = "b";
+  playerAllShipPosition.add([x, y].toString());
 }
 
-for (let y = 0; y < 10; y++) {
-  for (let x = 0; x < 10; x++) {
-    const div = document.createElement("div");
-    div.classList.add("cell");
+function renderPlayerBoard() {
+  playerContainer.textContent = "";
+  for (let y = 0; y < 10; y++) {
+    for (let x = 0; x < 10; x++) {
+      const div = document.createElement("div");
+      div.classList.add("cell");
 
-    if (computerBoard[x][y] == 1) {
-      div.classList.add("blue");
-    }
-
-    div.addEventListener("click", () => {
-      if (computerShipPosition.has([x, y].toString())) {
-        console.log("same");
-      } else {
-        console.log("not same");
+      if (playerBoard[x][y] == "b") {
+        div.classList.add("blue");
+      } else if (playerBoard[x][y] == "g") {
+        div.classList.add("green");
+      } else if (playerBoard[x][y] == "r") {
+        div.classList.add("red");
       }
 
-      console.log([x, y]);
-    });
-
-    computerContainer.append(div);
+      playerContainer.append(div);
+    }
   }
+}
+
+renderPlayerBoard();
+
+function attackRandomPosition(valid) {
+  const [x, y] = randomPosition();
+
+  if (valid === true) {
+  } else {
+    console.log("invalid");
+  }
+
+  console.log("valid");
+  if (playerAllShipPosition.has([x, y].toString())) {
+    if (playerAllMoves.has([x, y].toString())) {
+      // console.log("cannot hit twice");
+    } else {
+      playerBoard[x][y] = "r";
+      // console.log("hit");
+    }
+  } else {
+    playerBoard[x][y] = "g";
+    // console.log("miss");
+  }
+
+  renderPlayerBoard();
+  playerAllMoves.add([x, y].toString());
 }
--- randomShip.js
+++ randomShip.js
@@ -1,176 +1,114 @@
 import Ship from "./ship.js";
 import { randomPosition, randomDirection } from "./random.js";
 
-const occupied = new Set();
+function makeRandomShipPosition() {
+  const occupied = new Set();
+  function positionToString(position) {
+    return `${position[0]},${position[1]}`;
+  }
 
-function positionToString(position) {
-  return `${position[0]},${position[1]}`;
-}
+  function isPathClear(path) {
+    for (const position of path) {
+      if (occupied.has(positionToString(position))) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-function isPathClear(path) {
-  for (const position of path) {
-    if (occupied.has(positionToString(position))) {
-      return false;
+  function placeRandomCarrier() {
+    while (true) {
+      const randomP = randomPosition();
+      const randomD = randomDirection();
+
+      const carrier = new Ship("carrier", 5);
+      const startPosition = carrier.position(randomP);
+      const { endPosition, fullPath } = carrier.path(randomD);
+
+      if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
+        return fullPath;
+      }
     }
   }
-  return true;
-}
 
-function placeRandomCarrier() {
-  while (true) {
-    const randomP = randomPosition();
-    const randomD = randomDirection();
+  const randomCarrier = placeRandomCarrier();
+  randomCarrier.forEach((pos) => occupied.add(positionToString(pos)));
 
-    const carrier = new Ship("carrier", 5);
-    const startPosition = carrier.position(randomP);
-    const { endPosition, fullPath } = carrier.path(randomD);
+  function placeRandomBattleship() {
+    while (true) {
+      const randomP = randomPosition();
+      const randomD = randomDirection();
 
-    if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
-      return fullPath;
+      const battleship = new Ship("battleship", 4);
+      const startPosition = battleship.position(randomP);
+      const { endPosition, fullPath } = battleship.path(randomD);
+
+      if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
+        return fullPath;
+      }
     }
   }
-}
 
-const randomCarrier = placeRandomCarrier();
-randomCarrier.forEach((pos) => occupied.add(positionToString(pos)));
+  const randomBattleship = placeRandomBattleship();
+  randomBattleship.forEach((pos) => occupied.add(positionToString(pos)));
 
-function placeRandomBattleship() {
-  while (true) {
-    const randomP = randomPosition();
-    const randomD = randomDirection();
+  function placeRandomCruiser() {
+    while (true) {
+      const randomP = randomPosition();
+      const randomD = randomDirection();
 
-    const battleship = new Ship("battleship", 4);
-    const startPosition = battleship.position(randomP);
-    const { endPosition, fullPath } = battleship.path(randomD);
+      const battleship = new Ship("cruiser", 3);
+      const startPosition = battleship.position(randomP);
+      const { endPosition, fullPath } = battleship.path(randomD);
 
-    if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
-      return fullPath;
+      if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
+        return fullPath;
+      }
     }
   }
-}
 
-const randomBattleship = placeRandomBattleship();
-randomBattleship.forEach((pos) => occupied.add(positionToString(pos)));
+  const randomCruiser = placeRandomCruiser();
+  randomCruiser.forEach((pos) => occupied.add(positionToString(pos)));
 
-function placeRandomCruiser() {
-  while (true) {
-    const randomP = randomPosition();
-    const randomD = randomDirection();
+  function placeRandomSubmarine() {
+    while (true) {
+      const randomP = randomPosition();
+      const randomD = randomDirection();
 
-    const battleship = new Ship("cruiser", 3);
-    const startPosition = battleship.position(randomP);
-    const { endPosition, fullPath } = battleship.path(randomD);
+      const battleship = new Ship("submarine", 3);
+      const startPosition = battleship.position(randomP);
+      const { endPosition, fullPath } = battleship.path(randomD);
 
-    if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
-      return fullPath;
+      if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
+        return fullPath;
+      }
     }
   }
-}
 
-const randomCruiser = placeRandomCruiser();
-randomCruiser.forEach((pos) => occupied.add(positionToString(pos)));
+  const randomSubmarine = placeRandomSubmarine();
+  randomSubmarine.forEach((pos) => occupied.add(positionToString(pos)));
 
-function placeRandomSubmarine() {
-  while (true) {
-    const randomP = randomPosition();
-    const randomD = randomDirection();
+  function placeRandomDestroyer() {
+    while (true) {
+      const randomP = randomPosition();
+      const randomD = randomDirection();
 
-    const battleship = new Ship("submarine", 3);
-    const startPosition = battleship.position(randomP);
-    const { endPosition, fullPath } = battleship.path(randomD);
+      const battleship = new Ship("destroyer", 2);
+      const startPosition = battleship.position(randomP);
+      const { endPosition, fullPath } = battleship.path(randomD);
 
-    if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
-      return fullPath;
+      if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
+        return fullPath;
+      }
     }
   }
+
+  const randomDestroyer = placeRandomDestroyer();
+  randomDestroyer.forEach((pos) => occupied.add(positionToString(pos)));
+
+  return {
+    randomCarrier,
+  };
 }
 
-const randomSubmarine = placeRandomSubmarine();
-randomSubmarine.forEach((pos) => occupied.add(positionToString(pos)));
-
-function placeRandomDestroyer() {
-  while (true) {
-    const randomP = randomPosition();
-    const randomD = randomDirection();
-
-    const battleship = new Ship("destroyer", 2);
-    const startPosition = battleship.position(randomP);
-    const { endPosition, fullPath } = battleship.path(randomD);
-
-    if (endPosition[0] < 10 && endPosition[1] < 10 && isPathClear(fullPath)) {
-      return fullPath;
-    }
-  }
-}
-
-const randomDestroyer = placeRandomDestroyer();
-randomDestroyer.forEach((pos) => occupied.add(positionToString(pos)));
-
-function shipInformation() {
-  console.log("Carrier");
-  for (const element of randomCarrier) {
-    console.log(element);
-  }
-
-  console.log("Battleship");
-  for (const element of randomBattleship) {
-    console.log(element);
-  }
-
-  console.log("Cruiser");
-  for (const element of randomCruiser) {
-    console.log(element);
-  }
-
-  console.log("Submarine");
-  for (const element of randomSubmarine) {
-    console.log(element);
-  }
-
-  console.log("Destroyer");
-  for (const element of randomDestroyer) {
-    console.log(element);
-  }
-}
-
-const randomShip = [
-  ...randomCarrier,
-  ...randomBattleship,
-  ...randomCruiser,
-  ...randomSubmarine,
-  ...randomDestroyer,
-];
-
-function carrierRandomPosition() {
-  return randomCarrier;
-}
-
-function battleshipRandomPosition() {
-  return randomBattleship;
-}
-
-function cruiserRandomPosition() {
-  return randomCruiser;
-}
-
-function submarieRandomPosition() {
-  return placeSubmarine;
-}
-
-function destroyerRandomPosition() {
-  return placeDestroyer;
-}
-
-function randomShipPosition() {
-  return randomShip;
-}
-
-export {
-  shipInformation,
-  carrierRandomPosition,
-  battleshipRandomPosition,
-  cruiserRandomPosition,
-  submarieRandomPosition,
-  destroyerRandomPosition,
-  randomShipPosition,
-};
+export { makeRandomShipPosition };
