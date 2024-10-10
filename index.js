cd3a01705dacadb9e845d8e2aa679b2d08d071b9
andrei <andreimandrique3@gmail.com>
2024-10-10 13:10:29 +0800

    working gui

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import { randomShipPosition, shipInformation } from "./randomShip.js";
+import {
+  randomShipPosition,
+  carrierRandomPosition,
+  shipInformation,
+} from "./randomShip.js";
 
 const computerContainer = document.querySelector(".computer-container");
 
@@ -15,13 +19,33 @@ const computerBoard = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 9
 ];
 
+const randomShip = carrierRandomPosition();
+const computerShipPosition = new Set();
+
+for (const ship of randomShip) {
+  const x = ship[0];
+  const y = ship[1];
+  computerBoard[x][y] = 1;
+  computerShipPosition.add([x, y].toString());
+}
+
 for (let y = 0; y < 10; y++) {
   for (let x = 0; x < 10; x++) {
     const div = document.createElement("div");
     div.classList.add("cell");
 
+    if (computerBoard[x][y] == 1) {
+      div.classList.add("blue");
+    }
+
     div.addEventListener("click", () => {
-      console.log({ x, y });
+      if (computerShipPosition.has([x, y].toString())) {
+        console.log("same");
+      } else {
+        console.log("not same");
+      }
+
+      console.log([x, y]);
     });
 
     computerContainer.append(div);
