c48c1fea7e6d6b64c142a7d83f2e2ba62d02a841
andrei <andreimandrique3@gmail.com>
2024-10-10 15:52:56 +0800

    working gui

--- index.js
+++ index.js
@@ -7,69 +7,69 @@ function makeRandomShip() {
   return randomCarrier;
 }
 
-// const computerContainer = document.querySelector(".computer-container");
+const computerContainer = document.querySelector(".computer-container");
 const playerContainer = document.querySelector(".player-container");
 const btn = document.querySelector("button");
 
-// const computerBoard = [
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 0
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 2
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 3
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 4   Y
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 5
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 6
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 7
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 8
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 9
-// ];
+const computerBoard = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 0
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 2
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 3
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 4   Y
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 5
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 6
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 7
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 8
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 9
+];
 
-// const computerRandomShip = makeRandomShip();
-// const computerShipPosition = new Set();
-// const computerAllMoves = new Set();
+const computerRandomShip = makeRandomShip();
+const computerShipPosition = new Set();
+const computerAllMoves = new Set();
 
-// for (const ship of computerRandomShip) {
-//   const x = ship[0];
-//   const y = ship[1];
-//   computerBoard[x][y] = "b";
-//   computerShipPosition.add([x, y].toString());
-// }
+for (const ship of computerRandomShip) {
+  const x = ship[0];
+  const y = ship[1];
+  computerBoard[x][y] = "b";
+  computerShipPosition.add([x, y].toString());
+}
 
-// for (let y = 0; y < 10; y++) {
-//   for (let x = 0; x < 10; x++) {
-//     const div = document.createElement("div");
-//     div.classList.add("cell");
+for (let y = 0; y < 10; y++) {
+  for (let x = 0; x < 10; x++) {
+    const div = document.createElement("div");
+    div.classList.add("cell");
 
-//     if (computerBoard[x][y] == "b") {
-//       div.classList.add("blue");
-//     }
+    if (computerBoard[x][y] == "b") {
+      div.classList.add("blue");
+    }
 
-//     div.addEventListener("click", () => {
-//       if (computerShipPosition.has([x, y].toString())) {
-//         if (computerAllMoves.has([x, y].toString())) {
-//           // console.log("cannot attack the same position twice");
-//           attackRandomPosition(false);
-//         } else {
-//           div.classList.add("red");
-//           // console.log("hit");
-//           attackRandomPosition(true);
-//         }
-//       } else {
-//         if (computerAllMoves.has([x, y].toString())) {
-//           attackRandomPosition(false);
-//         } else {
-//           div.classList.add("green");
-//           // console.log("miss");
-//           attackRandomPosition(true);
-//         }
-//       }
+    div.addEventListener("click", () => {
+      if (computerShipPosition.has([x, y].toString())) {
+        if (computerAllMoves.has([x, y].toString())) {
+          // console.log("cannot attack the same position twice");
+          attackRandomPosition(false);
+        } else {
+          div.classList.add("red");
+          // console.log("hit");
+          attackRandomPosition(true);
+        }
+      } else {
+        if (computerAllMoves.has([x, y].toString())) {
+          attackRandomPosition(false);
+        } else {
+          div.classList.add("green");
+          // console.log("miss");
+          attackRandomPosition(true);
+        }
+      }
 
-//       computerAllMoves.add([x, y].toString());
-//     });
+      computerAllMoves.add([x, y].toString());
+    });
 
-//     computerContainer.append(div);
-//   }
-// }
+    computerContainer.append(div);
+  }
+}
 
 const playerBoard = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 0
@@ -102,12 +102,16 @@ function renderPlayerBoard() {
       const div = document.createElement("div");
       div.classList.add("cell");
 
-      if (playerBoard[x][y] == "b") {
-        div.classList.add("blue");
-      } else if (playerBoard[x][y] == "g") {
-        div.classList.add("green");
-      } else if (playerBoard[x][y] == "r") {
-        div.classList.add("red");
+      switch (playerBoard[x][y]) {
+        case "b":
+          div.classList.add("blue");
+          break;
+        case "g":
+          div.classList.add("green");
+          break;
+        case "r":
+          div.classList.add("red");
+          break;
       }
 
       playerContainer.append(div);
@@ -118,26 +122,21 @@ function renderPlayerBoard() {
 renderPlayerBoard();
 
 function attackRandomPosition(valid) {
-  const [x, y] = randomPosition();
-
   if (valid === true) {
-  } else {
-    console.log("invalid");
-  }
+    let x, y;
+    do {
+      [x, y] = randomPosition();
+    } while (playerAllMoves.has([x, y].toString()));
 
-  console.log("valid");
-  if (playerAllShipPosition.has([x, y].toString())) {
-    if (playerAllMoves.has([x, y].toString())) {
-      // console.log("cannot hit twice");
-    } else {
-      playerBoard[x][y] = "r";
+    if (playerAllShipPosition.has([x, y].toString())) {
       // console.log("hit");
+      playerBoard[x][y] = "r";
+    } else {
+      // console.log("miss");
+      playerBoard[x][y] = "g";
     }
-  } else {
-    playerBoard[x][y] = "g";
-    // console.log("miss");
-  }
 
-  renderPlayerBoard();
-  playerAllMoves.add([x, y].toString());
+    renderPlayerBoard();
+    playerAllMoves.add([x, y].toString());
+  }
 }
